dae882b09bf030cf486415ad27aab3ec6d9b976c
Githuzaifa <98671495+Githuzaifa@users.noreply.github.com>
2026-09-25 23:15:47 +0500

    fix signup problem

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -56,6 +56,7 @@ export default function LoginPage() {
         isClosable: true,
       });
       router.push("/");
+      router.refresh();
     }
     else{
         toast({
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -89,6 +89,7 @@ useEffect(() => {
         isClosable: true,
       });
       router.push("/");
+      router.refresh();
     }
   };
 
